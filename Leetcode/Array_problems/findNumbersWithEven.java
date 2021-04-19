5f8fad0114c98530faa9e66ffd27955ba0c4bde0
Mehir Pandit <mp91117@uga.edu>
2021-04-19 19:41:23 -0400

    Update findNumbersWithEven.java

--- Leetcode/Array_problems/findNumbersWithEven.java
+++ Leetcode/Array_problems/findNumbersWithEven.java
@@ -1,17 +1,17 @@
 //Find Numbers with Even Number of Digits
 
 public int findNumbers(int[] nums) {
-	int sol = 0;                            // Solution variable initialised to 0
-	for(int n : nums)                       // Iterate over each number
-		if(getNumberOfDigits(n) % 2 == 0)   // Call getNumberOfDigits which return the total number of digits in the number
-			sol++;                          // if even number of digits are returned we increment sol by 1
+	int sol = 0;                            
+	for(int n : nums)                       
+		if(getNumberOfDigits(n) % 2 == 0)  
+			sol++;                          
 	return sol;                             
 }
 
-public int getNumberOfDigits(int num) {
-	int count = 1;                          // Initialise count to 1, not 0 cause we are dividing num/10 in while loop
-	while((num /= 10) != 0)                 // Iterate until number is 0
-		count++;                            // Increment count until number is 0
+public int getDigits(int num) {
+	int count = 1;                          
+	while((num /= 10) != 0)                 
+		count++;                            
 	return count;
 }
 
